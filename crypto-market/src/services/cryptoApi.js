53ed3f2cc23ead4cce54d6fd77b745259969ad26
Prabakaran.M <karanpk0108@gmail.com>
2023-11-06 15:38:17 +0530

    installed-redux-fetched-data-from-crypto-api

---
+++ crypto-market/src/services/cryptoApi.js
@@ -0,0 +1,22 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  "X-RapidAPI-Key": "589f6b8738msh3fff3d32d964282p1f5f27jsnc2ae3241d5f2",
+  "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
+};
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const { useGetCryptosQuery } = cryptoApi;
