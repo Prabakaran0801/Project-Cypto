4dcded8f953c6aa19e4e39915b43c7357f27b597
Prabakaran.M <karanpk0108@gmail.com>
2023-11-09 10:27:44 +0530

    Added-responsive-navbar

--- crypto-market/src/components/Navbar.jsx
+++ crypto-market/src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Button, Menu, Typography, Avatar } from "antd";
 import { Link } from "react-router-dom";
 
@@ -11,6 +11,25 @@ import {
 } from "@ant-design/icons";
 import icon from "../images/logo.png";
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(null);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize < 768) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [setActiveMenu]);
+
   return (
     <div className="nav-container">
       <div className="logo-container">
@@ -18,28 +37,33 @@ const Navbar = () => {
         <Typography.Title level={2} className="logo">
           <Link to="/"> Crypto</Link>
         </Typography.Title>
-        {/* <Button className="menu-control-container">
-
-        </Button> */}
+        <Button
+          className="menu-control-container"
+          onClick={() => setActiveMenu(!activeMenu)}
+        >
+          <MenuOutlined />
+        </Button>
       </div>
-      <Menu theme="dark" selectedKeys={[]}>
-        <Menu.Item icon={<HomeOutlined />} key="home">
-          <Link to="/">Home</Link>
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item icon={<FundOutlined />} key="currencies">
-          <Link to="/cryptocurrencies">Currencies</Link>
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item icon={<MoneyCollectOutlined />} key="exchanges">
-          <Link to="/exchanges">Exchanges</Link>
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item icon={<BulbOutlined />} key="news">
-          <Link to="/news">News</Link>
-        </Menu.Item>
-        <Menu.Divider />
-      </Menu>
+      {activeMenu && (
+        <Menu theme="dark">
+          <Menu.Item icon={<HomeOutlined />} key="home">
+            <Link to="/">Home</Link>
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item icon={<FundOutlined />} key="currencies">
+            <Link to="/cryptocurrencies">Currencies</Link>
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item icon={<MoneyCollectOutlined />} key="exchanges">
+            <Link to="/exchanges">Exchanges</Link>
+          </Menu.Item>
+          <Menu.Divider />
+          <Menu.Item icon={<BulbOutlined />} key="news">
+            <Link to="/news">News</Link>
+          </Menu.Item>
+          <Menu.Divider />
+        </Menu>
+      )}
     </div>
   );
 };
